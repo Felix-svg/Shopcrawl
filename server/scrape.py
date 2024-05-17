75cc54bb5f333323096a3d8d41291f5c0fd20ff4
svg-den <sundaywanjiru@gmail.com>
2024-05-17 22:58:54 +0300

    updated

--- server/scrape.py
+++ server/scrape.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
-from server.convert_price import convert_price_to_float
+from convert_price import convert_price_to_float
 
 
 def search_amazon(product_name):
