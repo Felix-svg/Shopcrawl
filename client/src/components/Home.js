bdaf1e46e7dd62423e00a06ff38a9e5d0164d246
Felix Omondi <136954351+Felix-svg@users.noreply.github.com>
2024-05-19 15:33:30 +0300

    Responsive design, and randomise top products displayed to non logged in users

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import SearchBar from './SearchBar'; 
+import SearchBar from './SearchBar';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function Home({onLogout}) {
-    const navigate = useNavigate()
-    const handleLogout = () => {
-        localStorage.removeItem("access_token");
-        onLogout();
-        navigate("/")
-      };
-    return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', backgroundColor: '#90AEAD' }}>
-            <SearchBar />
-            <button onClick={handleLogout}>Logout</button>
-        </div>
-    );
+function Home({ onLogout }) {
+  const navigate = useNavigate();
+  
+  const handleLogout = () => {
+    localStorage.removeItem("access_token");
+    onLogout();
+    navigate("/");
+  };
+
+  return (
+    <div className="d-flex flex-column align-items-center justify-content-center vh-100 " style={{backgroundColor:"#90AEAD"}}>
+      <div className="mb-3 w-100" style={{ maxWidth: '600px' }}>
+        <SearchBar />
+      </div>
+      <button 
+        onClick={handleLogout} 
+        className="btn btn-dark mt-3"
+        style={{ width: '100%', maxWidth: '200px' }}
+      >
+        Logout
+      </button>
+    </div>
+  );
 }
 
 export default Home;
--- server/routes/products.py
+++ server/routes/products.py
@@ -1,16 +1,26 @@
 from flask import make_response
 from flask_restful import Resource
 from models.product import Product
+from random import sample
 
+
+# class Products(Resource):
+#     def get(self):
+#         products = []
+
+#         for product in Product.query.all():
+#             products.append(product.to_dict(rules=["-category"]))
+
+#         return make_response({"products": products}, 200)
 
 class Products(Resource):
     def get(self):
-        products = []
+        products = Product.query.all()
+        random_products = sample(products, min(len(products), 30))
 
-        for product in Product.query.all():
-            products.append(product.to_dict(rules=["-category"]))
+        products_data = [product.to_dict(rules=["-category"]) for product in random_products]
 
-        return make_response({"products": products}, 200)
+        return make_response({"products": products_data}, 200)
 
 
 class ProductByID(Resource):
