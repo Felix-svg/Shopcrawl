aa92abbc3449b39db80ffeb3552b1cb27f2e68a1
Felix Omondi <136954351+Felix-svg@users.noreply.github.com>
2024-05-20 19:06:33 +0300

    Add scraped data from jumia

--- server/convert_price.py
+++ server/convert_price.py
@@ -1,19 +1,24 @@
 import re
+
+
 def convert_price_to_float(price_str):
     if price_str is None:
         return None
     if isinstance(price_str, (int, float)):
         return float(price_str)
     if not isinstance(price_str, str):
-        raise TypeError(f"Expected string or bytes-like object, got '{type(price_str).__name__}'")
-    
+        raise TypeError(
+            f"Expected string or bytes-like object, got '{type(price_str).__name__}'"
+        )
+
     # Strip out any non-numeric characters except for decimal point
-    numeric_part = re.search(r'[\d,.]+', price_str)
+    numeric_part = re.search(r"[\d,.]+", price_str)
     if numeric_part:
-        numeric_part = numeric_part.group().replace(',', '')
+        numeric_part = numeric_part.group().replace(",", "")
         return float(numeric_part)
     else:
         raise ValueError(f"Could not convert string to float: {price_str}")
+
 
 def adjust_price(price_str):
     try:
@@ -23,5 +28,19 @@ def adjust_price(price_str):
     except IndexError:
         # If there's an issue with splitting the string, return None
         return None
-    
-    
+
+
+def convert_price_to_usd(price_str, exchange_rate=0.0071):
+    # Adjust the price string to handle ranges
+    adjusted_price_str = adjust_price(price_str)
+
+    # Convert the adjusted price string to float
+    price_ksh = convert_price_to_float(adjusted_price_str)
+
+    # Convert the price from KSh to USD
+    price_usd = price_ksh * exchange_rate
+
+    # Format the price in USD to two decimal places with a dollar sign
+    formatted_price_usd = f"${price_usd:.2f}"
+
+    return formatted_price_usd
