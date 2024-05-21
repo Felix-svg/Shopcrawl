39c9031e916f547e04cf0f25a9177d8d7f7bd202
Felix Omondi <136954351+Felix-svg@users.noreply.github.com>
2024-05-21 19:58:22 +0300

    Improve card styling and add a loading animation

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -13,35 +13,33 @@ const Products = () => {
       })
       .then((data) => {
         setProducts(data.products);
-        console.log(data);
       })
       .catch((error) => console.error(error));
   }, []);
 
   return (
     <div className="container">
-    <div className="row">
-      {products.map((product) => (
-        <div key={product.id} className="col-lg-4 col-md-6 mb-4 mt-4">
-          <div className="card h-100">
-            <img src={product.image_src} className="card-img-top" alt={product.name} />
-            <div className="card-body d-flex flex-column">
-              <h5 className="card-title">{product.name}</h5>
-              <p className="card-text">{product.description}</p>
-              <div className="mt-auto d-flex justify-content-center">
-                <a href={product.source} target="_blank" rel="noopener noreferrer" className="btn btn-link">
-                  Buy here
-                </a>
+      <div className="row">
+        {products.map((product) => (
+          <div key={product.id} className="col-lg-4 col-md-6 mb-4 mt-4">
+            <div className="card h-100">
+              <img
+                src={product.image_src}
+                className="card-img-top"
+                alt={product.name}
+              />
+              <div className="card-body">
+              <p className="card-title">{product.name}</p>
+                <p className="card-text">{product.description}</p>
+              </div>
+              <div className="card-footer">
+                <small className="text-muted">Price: {product.price}</small>
               </div>
             </div>
-            <div className="card-footer">
-              <small className="text-muted">Price: {product.price}</small>
-            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
-  </div>
   );
 };
 
