92210e7ad0f7d6eff7ab32b4f023d41a4e877484
Nyakio-Eva <evalkariuki@gmail.com>
2024-05-22 10:08:34 +0300

    Added test cases, edited product ranking to use the /search route instead of scraping directly

---
+++ server/fetchrankresults.py
@@ -0,0 +1,18 @@
+import requests
+
+def fetch_search_results(product_name):
+    # Make an HTTP request to the Flask server to fetch search results
+        response = requests.get("http://localhost:5000/search", params={"q": product_name})
+        
+        if response.status_code == 200:
+            search_results = response.json()
+            # Extract and combine products from search results
+            all_products = search_results['jumia'] + search_results['amazon'] + search_results['alibaba']
+            return all_products
+         
+        else:
+            print("Failed to fetch search results.")
+            return None
+        
+        
+        
