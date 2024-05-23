f7327a081b94e5d70839212ed672f5dfd4bc195d
Felix Omondi <136954351+Felix-svg@users.noreply.github.com>
2024-05-23 12:06:26 +0300

    Remove column constraints

--- server/models/product.py
+++ server/models/product.py
@@ -8,11 +8,11 @@ from datetime import datetime
 class Product(db.Model, SerializerMixin):
     __tablename__ = "products"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False)
-    price = db.Column(db.String(50), nullable=False)
-    image_src = db.Column(db.String(255))
+    name = db.Column(db.String, nullable=False)
+    price = db.Column(db.String, nullable=False)
+    image_src = db.Column(db.String)
     source = db.Column(
-        db.String(500)
+        db.String``
     )  # Store the source of the product (e.g., 'alibaba', 'amazon')
     rating = db.Column(db.String)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
